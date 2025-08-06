a50ccf1666c4326dba7256344f1438c59b733a1c
Eero <sksak75312@gmail.com>
2025-08-06 12:19:46 +0800

    feat: 新增 Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,41 @@ function App() {
   const { i18n } = useTranslation();
   const { pathname } = useLocation();
 
+  // 取得參數，使用 i18n 方法改變語言
   useEffect(() => {
     i18n.changeLanguage(lng);
   }, [lng, i18n, pathname]);
 
+  // 取得參數重新組成新的路由
   useEffect(() => {
     const processPath = pathname
       .split('/')
-      .filter((_, i) => i >= 2)
+      .filter((_, i) => i > 1)
       .join('/');
 
-    setTranslationPath( processPath ? `/${processPath}` : '');
+    setTranslationPath(processPath ? `/${processPath}` : '');
   }, [pathname]);
 
   return (
     <>
-      <div>
+      <div style={{ border: '1px solid white' }}>
+        <h2>Translation</h2>
         <Link to={`/zh-hant${translationPath}`}>
           <button type="button">繁體中文</button>
         </Link>
 
         <Link to={`/en${translationPath}`}>
           <button type="button">English</button>
+        </Link>
+      </div>
+
+      <div style={{ border: '1px solid black' }}>
+        <h2>Navigate</h2>
+        <Link to={`/${lng}`}>
+          <button type="button">Home</button>
+        </Link>
+        <Link to={`/${lng}/products`}>
+          <button type="button">Products</button>
         </Link>
       </div>
       <Outlet />
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,8 +3,11 @@ import App from '@/App';
 import Home from '@/pages/Home';
 import Products from '@/pages/Products';
 
+
+// 路由系統設定，優先設定 lng 動態 segment
 const router = createBrowserRouter([
   {
+
     path: '/:lng',
     element: <App />,
     children: [
